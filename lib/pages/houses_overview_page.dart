f6540840f76bb8a431d37eaf12ca2d6573ab10cc
Tom van Lieshout <tom.vanlieshout@kpn.com>
2023-05-21 15:26:52 +0200

    use bloc to start fetching houses and display basic cards

--- lib/pages/houses_overview_page.dart
+++ lib/pages/houses_overview_page.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/widgets.dart';
-import 'package:funda_lite/api/service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:funda_lite/bloc/bloc.dart';
+import 'package:funda_lite/bloc/events.dart';
+import 'package:funda_lite/bloc/states.dart' as bloc;
+import 'package:funda_lite/models/house.dart';
 
 class HousesOverviewPage extends StatefulWidget {
   const HousesOverviewPage({super.key});
@@ -9,17 +13,45 @@ class HousesOverviewPage extends StatefulWidget {
 }
 
 class _HousesOverviewPageState extends State<HousesOverviewPage> {
-  final Service _service = Service();
-  late final response;
-
+  late FundaBloc _bloc;
   @override
   void initState() {
     super.initState();
-    _service.getHousesByCity('Nijmegen').then((value) => response = value);
+    _bloc = BlocProvider.of(context);
+    _bloc.add(LoadHouses('Nijmegen'));
+  }
+
+  @override
+  void dispose() {
+    _bloc.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: BlocBuilder(
+        bloc: _bloc,
+        builder: (context, state) {
+          if (state is bloc.InitialState) return Container();
+          if (state is bloc.Loading) return const Center(child: CircularProgressIndicator());
+          if (state is bloc.HousesLoaded) return _buildHousesCards(state.houses);
+          return Container(); // ErrorState
+        },
+      ),
+    );
+  }
+
+  _buildHousesCards(List<House?> houses) {
+    final children = houses
+        .map((house) => Card(
+              child: Text(house!.address),
+            ))
+        .toList();
+    return Center(
+      child: Column(
+        children: children,
+      ),
+    );
   }
 }
