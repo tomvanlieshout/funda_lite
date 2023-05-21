1bfde4565bc71b21e07ea2d54fac5c2c39709b00
Tom van Lieshout <tom.vanlieshout@kpn.com>
2023-05-21 14:58:56 +0200

    Create new base model: House. Rename existing House to HouseDetails

--- lib/model/house.dart
+++ lib/model/house.dart
@@ -1,42 +1,13 @@
 class House {
-  final Map _sourceMap;
+  final String address;
+  final String postalCode;
+  final String price;
+  final String bedrooms;
 
-  const House.fromMap(this._sourceMap);
-
-  List<String> get photos => _extractPhotos();
-  String? get adUrl => _sourceMap['URL'];
-
-  String? get streetAndHouseNo => _sourceMap['Adres'];
-  String? get postalCode => _sourceMap['Postcode'];
-  String? get city => _sourceMap['Plaats'];
-  String? get price => '€${_sourceMap['Prijs']?['Koopprijs'].toString().replaceRange(3, 3, '.')}'; // Add euro-sign and '.' after first three digits
-  String? get pricePerSqMeter => '€${eurosPerSqMeter.round().toString().replaceRange(1, 1, '.')}';
-  num? get squareMeters => _sourceMap['WoonOppervlakte'];
-  num get bedrooms => _sourceMap['AantalKamers'] ?? 0;
-  String? get energyGrade => _sourceMap['Energielabel']?['Label'];
-
-  String? get description => _sourceMap['VolledigeOmschrijving'];
-  String? get constructionYear => _sourceMap['Bouwjaar'];
-  String? get houseType => _sourceMap['SoortWoning'];
-  String? get brokerName => _sourceMap['Makelaar'];
-  String? get brokerPhoneNumber => _sourceMap['MakelaarTelefoon'];
-
-  num get eurosPerSqMeter => (_sourceMap['Prijs']?['Koopprijs'] ?? 0) / (squareMeters ?? 1);
-  List<String> _extractPhotos() {
-    final result = <String>[];
-
-    for (var map in _sourceMap['Media']) {
-      int numberOfResolutions = map['MediaItems'].length;
-      result.add(map['MediaItems'][numberOfResolutions - 1]['Url']);
-    }
-
-    return result;
-  }
-
-  Map asMap() => {..._sourceMap};
-
-  House copyWith(Map<String, dynamic> newData) => House.fromMap({...asMap(), ...newData});
-
-  @override
-  String toString() => asMap().values.join(',');
+  const House({
+    required this.address,
+    required this.postalCode,
+    required this.price,
+    required this.bedrooms,
+  });
 }
---
+++ lib/model/house_details.dart
@@ -0,0 +1,42 @@
+class HouseDetails {
+  final Map _sourceMap;
+
+  const HouseDetails.fromMap(this._sourceMap);
+
+  List<String> get photos => _extractPhotos();
+  String? get adUrl => _sourceMap['URL'];
+
+  String? get streetAndHouseNo => _sourceMap['Adres'];
+  String? get postalCode => _sourceMap['Postcode'];
+  String? get city => _sourceMap['Plaats'];
+  String? get price => '€${_sourceMap['Prijs']?['Koopprijs'].toString().replaceRange(3, 3, '.')}'; // Add euro-sign and '.' after first three digits
+  String? get pricePerSqMeter => '€${eurosPerSqMeter.round().toString().replaceRange(1, 1, '.')}';
+  num? get squareMeters => _sourceMap['WoonOppervlakte'];
+  num get bedrooms => _sourceMap['AantalKamers'] ?? 0;
+  String? get energyGrade => _sourceMap['Energielabel']?['Label'];
+
+  String? get description => _sourceMap['VolledigeOmschrijving'];
+  String? get constructionYear => _sourceMap['Bouwjaar'];
+  String? get houseType => _sourceMap['SoortWoning'];
+  String? get brokerName => _sourceMap['Makelaar'];
+  String? get brokerPhoneNumber => _sourceMap['MakelaarTelefoon'];
+
+  num get eurosPerSqMeter => (_sourceMap['Prijs']?['Koopprijs'] ?? 0) / (squareMeters ?? 1);
+  List<String> _extractPhotos() {
+    final result = <String>[];
+
+    for (var map in _sourceMap['Media']) {
+      int numberOfResolutions = map['MediaItems'].length;
+      result.add(map['MediaItems'][numberOfResolutions - 1]['Url']);
+    }
+
+    return result;
+  }
+
+  Map asMap() => {..._sourceMap};
+
+  HouseDetails copyWith(Map<String, dynamic> newData) => HouseDetails.fromMap({...asMap(), ...newData});
+
+  @override
+  String toString() => asMap().values.join(',');
+}
